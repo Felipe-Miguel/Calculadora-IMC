44669226aced297ab2efad975bfb8bb85dc7aef5
Felipe Miguel Ortega <83983464+Felipe-Miguel@users.noreply.github.com>
2023-02-23 15:42:08 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import {
   StyleSheet,
-  Button,
   Text,
   TextInput,
   View,
   Platform,
-  Image,
   ImageBackground,
   TouchableOpacity,
-  Switch,
-  ActivityIndicator,
-  ScrollView,
 } from "react-native";
 import React from "react";
 export default class App extends React.Component {
@@ -36,30 +31,29 @@ export default class App extends React.Component {
   }
   render() {
     return (
-      <View style={styles.container}>
-               {" "}
+      <ImageBackground style={styles.container} source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyIYh49Hw53XU0ZjwrIk82-ShgL9IeIz7RXpNwHavhQgn24lriDvZarzGwDmWSI-MVyvg&usqp=CAU'}} resizeMode="cover">
+      <View style={styles.geral}>
+        <Text style={styles.text}>Calculadora de IMC</Text>
         <TextInput
           id="input1"
           placeholder="Peso"
           style={styles.textInput}
           onChangeText={(valor1) => this.setState({ valor1 })}
         />
-               {" "}
         <TextInput
           id="input2"
           placeholder="Altura"
           style={styles.textInput}
           onChangeText={(valor2) => this.setState({ valor2 })}
         />
-                      {" "}
         <TouchableOpacity
           onPress={() => this.calculaIMC()}
           style={styles.touchableopacity}
-        >
-                  <Text>Calcular</Text>       {" "}
+        ><Text>Calcular</Text>   
         </TouchableOpacity>
-             {" "}
       </View>
+      </ImageBackground>
+    
     );
   }
 }
@@ -74,7 +68,10 @@ const styles = StyleSheet.create({
     fontSize: 25,
     ...Platform.select({ ios: { color: "red" }, android: { color: "green" } }),
   },
-  textColor: { color: "black" },
+  geral:{
+    textAlign:'center',
+    marginTop: 250,
+  },
   textInput: {
     backgroundColor: "white",
     borderWidth: 2,
@@ -92,4 +89,8 @@ const styles = StyleSheet.create({
     marginTop: 10,
     borderWidth: 1,
   },
+  text:{
+    color: 'white',
+     fontWeight: 'bold',
+  }
 });
